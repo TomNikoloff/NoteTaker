afb31472c1015460bd8cf672c8f579313150544c
TomNikoloff <tomnikoloff@hotmail.co.uk>
2022-02-12 17:37:39 +0000

    Updated to fit portfolio

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ const addBtn = document.querySelector('.btn');
 const textarea = document.querySelector('textarea');
 const noteCon = document.querySelector('.noteContainer');
 const placeholder = document.querySelector('#placeholder');
+const modal = document.querySelector('#modal');
+const modalText = document.querySelector('#modalText');
+const close = document.querySelector('.close');
+let count = 0;
 
 function addNote() {
+    count++;
     let h3 = document.createElement('h3');
-    h3.appendChild(document.createTextNode('Note 1'));
+    h3.appendChild(document.createTextNode('Note ' + count));
 
     let p = document.createElement('p');
-    p.appendChild(document.createTextNode(textarea.value));
+    let note = document.createTextNode(textarea.value);
+    p.appendChild(note);
 
     let btn = document.createElement('button');
     btn.appendChild(document.createTextNode('View Detail'));
+
 
     let div = document.createElement('div');
     div.classList.add('noteArea');
@@ -23,6 +30,30 @@ function addNote() {
     placeholder.classList.add('delete');
 
     noteCon.appendChild(div);
+
+    btn.addEventListener('click', displayModal);
+
+    function displayModal() {
+        modal.style.display = "block";
+        let x = note.textContent
+        modalText.textContent = x;
+    }
+
+
+    close.addEventListener('click', closeModal);
+
+    function closeModal() {
+        modal.style.display = "none"
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
 }
 
+
+
 addBtn.addEventListener('click', addNote);
+
